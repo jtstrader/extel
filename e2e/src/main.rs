dad0feff68855a0631ad018690748fb308704f73
jtstrader <jacob.thomas.strader@gmail.com>
2025-09-05 11:52:59 -0400

    chore: Cleanup project in prep for xtl

--- e2e/src/main.rs
+++ e2e/src/main.rs
@@ -1,19 +1,8 @@
-pub mod tests;
+fn main() {}
 
-use extel::prelude::*;
-use tests::{
-    command_tests::CommandTestSuite, math_tests::MathTestSuite,
-    unsupported_errors::UnsupportedErrorTestSuite, utf8_tests::Utf8TestSuite,
-};
+/* Test macro defs here */
 
-fn main() {
-    run_tests::<MathTestSuite>();
-    run_tests::<CommandTestSuite>();
-    run_tests::<Utf8TestSuite>();
-    run_tests::<UnsupportedErrorTestSuite>();
-}
 
-/// Run test set with default configuration.
-fn run_tests<S: RunnableTestSet>() {
-    S::run(TestConfig::default());
+fn foo() {
+    let x: usize = 2;
 }
--- e2e/src/tests/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod command_tests;
-pub mod math_tests;
-pub mod unsupported_errors;
-pub mod utf8_tests;
